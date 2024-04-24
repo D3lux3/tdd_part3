a9bf22032cb320e2149c807c217c1c47fbd73e36
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-24 17:11:56 +0300

    test case for year incrementing

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,6 +1,6 @@
 import { describe, test, vi } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/testable1.ts";
+import { daysUntilChristmas, getNextChristmasDay, getTodayDate } from "../src/testable1.ts";
 import { afterEach, beforeEach } from "node:test";
 
 describe("Untestable 1: days until Christmas day", () => {
@@ -12,9 +12,16 @@ describe("Untestable 1: days until Christmas day", () => {
     vi.useRealTimers();
   })
 
+  test("year gets incremented if over this years christmas day", () => {
+    const dayAfterXmas = new Date(2024, 11, 26);
+    vi.setSystemTime(dayAfterXmas);
+    expect(getNextChristmasDay(dayAfterXmas).getFullYear()).equal(2025)
+  })
+
   test("364 days when it's day after christmas day", () => {
     const dayAfterXmas = new Date(2024, 11, 26);
     vi.setSystemTime(dayAfterXmas);
     expect(daysUntilChristmas()).to.equal(364)
   });
+
 });
