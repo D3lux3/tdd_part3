3c1bcab26ae40fce75fc613f993e48050fbccc04
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-24 18:20:44 +0300

    Tests for csv parsing

--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -1,18 +1,27 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { parsePeopleCsv, readFile } from "../src/testable3.ts";
+import { parsePeopleCsv, readFile, parseCSV } from "../src/testable3.ts";
 
 const testData = ['Loid,Forger,,Male', 'Anya,Forger,6,Female', 'Yor,Forger,27,Female'];
+const testDataFormatted = testData.join("\n").toString();
 
 describe("Untestable 3: CSV file parsing", () => {
 
   test("Reads files on system", async () => {
     try {
       const file = await readFile("./test/untestable3_input.csv");
-      expect(file).equal(testData.join("\n").toString());
+      expect(file).equal(testDataFormatted);
     } catch (e) {
       expect(e).equal(undefined);
     }
   });
+
+  test("Parses csv into records", () => {
+    const parsed = parseCSV(testDataFormatted);
+    const testDataNames = testData.map((line) => line.split(',')[0])
+    const parsedNames = parsed.map((record) => record[0])
+    expect(parsed.length).equal(3);
+    expect(parsedNames).to.deep.equal(testDataNames);
+  })
 
 });
