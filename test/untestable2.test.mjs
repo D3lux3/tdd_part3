36356d4c434e90a1fe5c1da7ee1bb7cccdb24074
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-24 17:37:02 +0300

    Added first test case

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -5,7 +5,6 @@ import { diceHandValue } from "../src/testable2.ts";
 describe("Untestable 2: a dice game", () => {
   test("returns 101 if dices are the same", () => {
     const fakeDiceRoll = () => 1;
-
     expect(diceHandValue(fakeDiceRoll)).equal(101)
   });
 });
