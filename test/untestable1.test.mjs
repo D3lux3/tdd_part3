8f4d2e2c8265b605980e17fb5dcba5b173ad520e
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-24 17:02:16 +0300

    Added mock clock

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/testable1.ts";
 import { afterEach, beforeEach } from "node:test";
 
-describe("Untestable 1: days until Christmas", () => {
+describe("Untestable 1: days until Christmas day", () => {
   beforeEach(() => {
     vi.useFakeTimers();
   });
@@ -12,8 +12,9 @@ describe("Untestable 1: days until Christmas", () => {
     vi.useRealTimers();
   })
 
-  test("364 days when it's day after christmas", () => {
-    // TODO: write proper tests
+  test("364 days when it's day after christmas day", () => {
+    const dayAfterXmas = new Date(2024, 11, 26);
+    vi.setSystemTime(dayAfterXmas);
     expect(daysUntilChristmas()).to.equal(364)
   });
 });
