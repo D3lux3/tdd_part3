30c75b42261ae4631b73fc6aa2c24dede296001f
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-24 18:46:36 +0300

    Added rest of the functionality

--- src/testable3.ts
+++ src/testable3.ts
@@ -1,6 +1,13 @@
 import { readFile as rf } from "node:fs/promises";
 import { parse } from "csv-parse/sync";
 
+interface ParsedRecords {
+    firstName: string;
+    lastName: string;
+    gender: string;
+    age: number | undefined;
+}
+
 export const readFile = async (filePath: string) => {
     return await rf(filePath, { encoding: "utf8" });
 }
@@ -12,21 +19,17 @@ export const parseCSV = (csvData: string) => {
     });
 }
 
-export async function parsePeopleCsv(filePath) {
-    const csvData = await rf(filePath, { encoding: "utf8" });
-    const records = parse(csvData, {
-        skip_empty_lines: true,
-        trim: true,
-    });
-    return records.map(([firstName, lastName, age, gender]) => {
-        const person = {
-            firstName,
-            lastName,
-            gender: gender.charAt(0).toLowerCase(),
-        };
-        if (age !== "") {
-            person.age = parseInt(age);
-        }
-        return person;
-    });
+export function parsePeopleCsv(csvRecords: string[]) {
+    return csvRecords.map(([firstName, lastName, age, gender]) => ({
+        firstName,
+        lastName,
+        gender: gender.charAt(0).toLowerCase(),
+        age: age !== "" ? parseInt(age) : undefined
+    }));
+}
+
+export async function readAndParseCSV(filePath: string, readFile: (csvData: string) => Promise<string>, parseCSV: (csvData: string) => string[], parsePeopleCsv: (csvRecords: string[]) => ParsedRecords[]) {
+    const csvData = await readFile(filePath);
+    const records = parseCSV(csvData);
+    return parsePeopleCsv(records);
 }
