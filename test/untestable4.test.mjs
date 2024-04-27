023e7cb376114af97e241305ba5a0e7daf78239a
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-27 20:54:24 +0300

    Added teardown for tests

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,14 +1,20 @@
-import { afterEach, beforeEach, describe, test } from "vitest";
+import { afterAll, afterEach, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
 
 describe("Untestable 4: enterprise application", () => {
   let service;
+  const daoInstance = UserDaoInstance;
+
   beforeEach(() => {
     service = new PasswordService();
   });
 
   afterEach(() => {
     PostgresUserDao.getInstance().close();
+  });
+
+  afterAll(() => {
+    service.getInstance().dropTables();
   });
 
   test("todo", async () => {
