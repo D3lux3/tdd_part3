525d3c660033be1ed19227c228fec968c5691a71
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-27 22:05:00 +0300

    Added function for creating test user to database.

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -3,6 +3,11 @@ import 'dotenv/config'
 import { PasswordService, PostgresUserDao } from "../src/testable4.ts";
 import { expect } from "chai";
 
+const addTestUserToDatabase = async (dao) => {
+  const user = { userId: 1, passwordHash: "foo" };
+  await dao.save(user);
+}
+
 describe("Untestable 4: enterprise application", () => {
   let service;
 
@@ -26,23 +31,19 @@ describe("Untestable 4: enterprise application", () => {
 
   test("Users can be saved and fetched from the database in PostgresUserDao", async () => {
     const dao = service.users;
-    const user = { userId: 1, passwordHash: "foo" };
-    await dao.save(user);
-
+    await addTestUserToDatabase(dao);
     const fetchedUser = await dao.getById(1);
-    expect(fetchedUser).deep.equal(user);
+    expect(fetchedUser).deep.equal({ userId: 1, passwordHash: "foo" });
   });
 
   test("Saving existing user updates its values in the database in PostgresUserDao", async () => {
     const dao = service.users;
-    const user = { userId: 1, passwordHash: "foo" };
-    await dao.save(user);
+    await addTestUserToDatabase(dao);
 
     const updatedUser = { userId: 1, passwordHash: "bar" };
     await dao.save(updatedUser);
 
     const fetchedUser = await dao.getById(1);
-
     expect(fetchedUser).deep.equal(updatedUser);
   });
 });
