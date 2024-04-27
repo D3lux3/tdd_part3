a7a714d8a312ff281977da3238a4a1da0d87ace8
D3lux3 <dani.haapaniemi@hotmail.com>
2024-04-27 22:40:41 +0300

    Added rests of the test cases

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,11 +1,17 @@
 import { afterEach, beforeEach, describe, test } from "vitest";
+import argon2 from "@node-rs/argon2";
 import 'dotenv/config'
 import { PasswordService, PostgresUserDao } from "../src/testable4.ts";
 import { expect } from "chai";
 
-const addTestUserToDatabase = async (dao) => {
-  const user = { userId: 1, passwordHash: "foo" };
+const addTestUserToDatabase = async (dao, passwordHash) => {
+  const user = { userId: 1, passwordHash: passwordHash };
   await dao.save(user);
+}
+
+const testHasher = {
+  verifySync: (hash, password) => Buffer.from(hash, 'base64').toString('ascii') == password,
+  hashSync: (password) => Buffer.from(password).toString('base64'),
 }
 
 describe("Untestable 4: enterprise application", () => {
@@ -31,14 +37,17 @@ describe("Untestable 4: enterprise application", () => {
 
   test("Users can be saved and fetched from the database in PostgresUserDao", async () => {
     const dao = service.users;
-    await addTestUserToDatabase(dao);
+    const userPasswordHash = argon2.hashSync("foo");
+    await addTestUserToDatabase(dao, userPasswordHash);
     const fetchedUser = await dao.getById(1);
-    expect(fetchedUser).deep.equal({ userId: 1, passwordHash: "foo" });
+    expect(fetchedUser).deep.equal({ userId: 1, passwordHash: userPasswordHash });
   });
 
   test("Saving existing user updates its values in the database in PostgresUserDao", async () => {
     const dao = service.users;
-    await addTestUserToDatabase(dao);
+    const userPasswordHash = argon2.hashSync("foo");
+
+    await addTestUserToDatabase(dao, userPasswordHash);
 
     const updatedUser = { userId: 1, passwordHash: "bar" };
     await dao.save(updatedUser);
@@ -46,4 +55,23 @@ describe("Untestable 4: enterprise application", () => {
     const fetchedUser = await dao.getById(1);
     expect(fetchedUser).deep.equal(updatedUser);
   });
+
+  test("PasswordService can change users password", async () => {
+    const dao = service.users;
+    const userPasswordHash = testHasher.hashSync("foo");
+    await addTestUserToDatabase(dao, userPasswordHash);
+    await service.changePassword(1, "foo", "bar", testHasher);
+
+    const fetchedUser = await dao.getById(1);
+    expect(fetchedUser).deep.equal({ userId: 1, passwordHash: testHasher.hashSync("bar") });
+  })
+
+  test("PasswordService does change users password if old password is wrong", async () => {
+    const dao = service.users;
+    const userPasswordHash = testHasher.hashSync("foo");
+    await addTestUserToDatabase(dao, userPasswordHash);
+    expect(async () => await service.changePassword(1, "wrongPassword", "bar", testHasher)).to.throw;
+    const fetchedUser = await dao.getById(1);
+    expect(fetchedUser).deep.equal({ userId: 1, passwordHash: testHasher.hashSync("foo") });
+  })
 });
